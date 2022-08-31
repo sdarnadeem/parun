1abe92b247aee7bb45f508141e20857d46db156d
Nasyx Nadeem <sdarnadeem@gmail.com>
2022-08-31 13:01:55 +0530

    implement main application class

--- app/src/main/java/dev/nasyxnadeem/parun/ParunApplication.kt
+++ app/src/main/java/dev/nasyxnadeem/parun/ParunApplication.kt
@@ -1,4 +1,8 @@
 package dev.nasyxnadeem.parun
 
-class ParunApplication {
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ParunApplication: Application() {
 }
