be2d36275f1b4bb6097fe5cc1ffecdcc0d418b06
Nasyx Nadeem <sdarnadeem@gmail.com>
2022-08-31 12:56:04 +0530

    add required deps

--- app/src/main/java/dev/nasyxnadeem/parun/MainActivity.kt
+++ app/src/main/java/dev/nasyxnadeem/parun/MainActivity.kt
@@ -16,28 +16,22 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ParunTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+           ParunApp()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun ParunApp() {
     ParunTheme {
-        Greeting("Android")
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+
+        }
     }
 }
+
+
