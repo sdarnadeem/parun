033145a57fe487f7f365e26b3e9d5853ffb8c690
Nasyx Nadeem <sdarnadeem@gmail.com>
2022-08-31 14:40:11 +0530

    add animations to splash screen

--- app/src/main/java/dev/nasyxnadeem/parun/screens/splash/SplashScreen.kt
+++ app/src/main/java/dev/nasyxnadeem/parun/screens/splash/SplashScreen.kt
@@ -1,11 +1,15 @@
 package dev.nasyxnadeem.parun.screens.splash
 
+import androidx.compose.animation.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -13,16 +17,24 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import dev.nasyxnadeem.parun.navigation.ParunScreens
 
 
-@Preview(showBackground = true)
 @Composable
-fun SplashScreen(navController: NavController? = null) {
+fun SplashScreen(navController: NavController) {
+
+    val color = remember { Animatable(Color.Black) }
+
+    // animate to red
+    LaunchedEffect(true) {
+        color.animateTo(Color.Green, animationSpec = tween(2000))
+        navController.navigate(ParunScreens.LoginScreen.name)
+    }
+
     val primaryTextStyle = TextStyle(
-        color = Color.Black,
+        color = color.value,
         fontWeight = FontWeight.Bold,
         fontFamily = FontFamily.SansSerif,
         fontSize = 70.sp
